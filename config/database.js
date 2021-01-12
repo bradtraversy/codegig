f93969146a6e2c7a7e406fb60469c7338ea659c2
zubair-saif <zubairsaif700@gmail.com>
2021-01-12 10:11:24 +0500

    removed unused code

--- config/database.js
+++ config/database.js
@@ -1,10 +1,6 @@
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize(process.env.POSTGRES_URI, {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: 0,
-
   pool: {
     max: 5,
     min: 0,
